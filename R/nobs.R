24fe261f71ff97dfb80a7253d6406dc1802c7949
Mauricio Vargas <mvargas@dcc.uchile.cl>
2021-04-03 17:35:10 -0300

    correct tests (i.e. glm family)

--- R/nobs.R
+++ R/nobs.R
@@ -1,3 +1,5 @@
-nobs.eglm <- function(object, use.fallback = FALSE, ...) {
+#' @export
+#' @keywords internal
+nobs.elm <- function(object, use.fallback = FALSE, ...) {
   if (!is.null(w <- object$weights)) sum(w != 0) else object$n
 }
--- tests/testthat/test-stats.R
+++ tests/testthat/test-stats.R
@@ -1,8 +1,24 @@
-test_that("functions in R/integration-stats.R (except summary()) return the same as glm", {
+test_that("functions from stats package (except summary()) return the same as lm", {
+  m1 <- lm(mpg ~ wt, data = mtcars)
+  m2 <- glm(mpg ~ wt, data = mtcars)
+
+  expect_equal(fitted(m1), fitted(m2))
+  expect_equal(coef(m1), coef(m2))
+  expect_equal(vcov(m1), vcov(m2))
+  expect_equal(deviance(m1), deviance(m2))
+  expect_equal(nobs(m1), nobs(m2))
+  expect_equal(model.matrix(m1), model.matrix(m2))
+})
+
+test_that("functions from stats package (except summary()) return the same as glm", {
   m1 <- glm(mpg ~ wt, family = gaussian(), data = mtcars)
   m2 <- eglm(mpg ~ wt, family = gaussian(), data = mtcars)
 
-  # expect_equal(family(m1), family(m2))
+  fm1 <- family(m1)
+  fm2 <- family(m2)
+
+  expect_equal(fm1$family, fm2$family)
+  expect_equal(fm1$link, fm2$link)
   expect_equal(fitted(m1), fitted(m2))
   expect_equal(coef(m1), coef(m2))
   expect_equal(vcov(m1), vcov(m2))
